c479501fa8aecf2fc14b98992e54454dba2cd44c
RobinKumar5986 <72180986+RobinKumar5986@users.noreply.github.com>
2022-04-17 18:29:00 +0530

    Update program 9.c

--- program 9.c
+++ program 9.c
@@ -1,181 +1,181 @@
-#include <stdio.h>
+//-----polnomial additon------//
 #include <stdlib.h>
-
-typedef struct
+#include <stdio.h>
+//------structure------//
+typedef struct poly
 {
-    int coeff,x_exp,y_exp,z_exp;
-    struct polynode *link;
+    int coeff,x_expo,y_expo,z_expo;
+    struct poly *link;
 }polynode;
-
-polynode *createnode(int coeff,int x_exp,int y_exp,int z_exp)
+//-------create function-------//
+polynode*create(int coeff,int x_expo,int y_expo,int z_expo)
 {
     polynode *node;
     node=(polynode*)malloc(sizeof(polynode));
     node->coeff=coeff;
-    node->x_exp=x_exp;
-    node->y_exp=y_exp;
-    node->z_exp=z_exp;
+    node->x_expo=x_expo;
+    node->y_expo=y_expo;
+    node->z_expo=z_expo;
     node->link=NULL;
     return node;
 }
-
-polynode *attachnode(polynode *node,polynode *poly)
+//-------attach function-------//
+polynode*attachnode(polynode*node,polynode*poly)
 {
-     polynode *cur;
-	 cur=poly->link;
-	 while(cur->link!=poly)
-	 {
-		 cur=cur->link;
-	 }
-	 cur->link=node;
-	 node->link=poly;
-	 return poly;
+    polynode *cur;
+    cur=poly->link;
+    while(cur->link!=poly)
+    {
+        cur=cur->link;
+    }
+    cur->link=node;
+    node->link=poly;
+    return poly;
 }
-
+//-------read function-------//
 polynode *readpoly()
 {
-    polynode *poly=(polynode*)malloc(sizeof(polynode));
+    polynode*poly=(polynode*)malloc(sizeof(polynode));
     poly->link=poly;
+    int coef,x_exp,y_exp,z_exp;
     int n,i;
-    int coeff,x_exp,y_exp,z_exp;
-    polynode *temp;
-    printf("Enter number of terms\n");
+    polynode*temp;
+    printf("\n\tEnter the number of terms:");
     scanf("%d",&n);
-    for (i=0;i<n;i++)
+    for(i=0;i<n;i++)
     {
-        printf("Term %d:\n",i+1);
-        printf("Enter the coefficient\n");
-        scanf("%d",&coeff);
-        printf("Enter exponent values for x,y and z\n");
-        scanf("%d%d%d",&x_exp,&y_exp,&z_exp);
-        temp=createnode(coeff,x_exp,y_exp,z_exp);
-        poly=attachnode(temp,poly);
+        printf("\n\tEnter the tern %d:",i+1);
+        printf("\nEnter coeff x,y and z:\n");
+        scanf("%d%d%d%d",&coef,&x_exp,&y_exp,&z_exp);
+        temp=create(coef,x_exp,y_exp,z_exp);
+        attachnode(temp,poly);
     }
     return poly;
 }
-
-void display(polynode *poly)
+//-------display-------//
+void display(polynode*poly)
 {
-    polynode *cur;
+    polynode*cur;
     cur=poly->link;
     while(cur!=poly)
     {
         if(cur->coeff>=0)
-            printf("+%dx^%dy^%dz^%d",cur->coeff,cur->x_exp,cur->y_exp,cur->z_exp);
+            printf("+%dx^%dy^%dz^%d",cur->coeff,cur->x_expo,cur->y_expo,cur->z_expo);
         else
-            printf("%dx^%dy^%dz^%d",cur->coeff,cur->x_exp,cur->y_exp,cur->z_exp);
+            printf("%dx^%dy^%dz^%d",cur->coeff,cur->x_expo,cur->y_expo,cur->z_expo);
         cur=cur->link;
     }
 }
-
+//-------power function-------//
 int power(int x,int n)
 {
-    if (n==0)
+    if(n==0)
         return 1;
     return x*power(x,n-1);
 }
-
-void evaluate(polynode *poly1)
+//-------evaluate function-------//
+void evaluate(polynode*poly1)
 {
-    polynode *poly;
-    int x,y,z,xval,yval,zval,res=0;
+    polynode*poly;
+    int x,y,z, xval,yval,zval,sum=0;
     poly=poly1->link;
-    printf("\nEnter the values x, y and z:\n");
+    printf("\nEnter the value of x,y and z:\n");
     scanf("%d%d%d",&x,&y,&z);
-    while (poly!=poly1)
+    while(poly!=poly1)
     {
-        xval=power(x,poly->x_exp);
-        yval=power(y,poly->y_exp);
-        zval=power(z,poly->z_exp);
-        res+=poly->coeff*xval*yval*zval;
+        xval=power(x,poly->x_expo);
+        yval=power(y,poly->y_expo);
+        zval=power(z,poly->z_expo);
+        sum=sum+(poly->coeff*xval*yval*zval);
         poly=poly->link;
     }
-    printf("Result=%d\n",res);
+    printf("\nResult:%d",sum);
 }
-
-polynode *addpoly(polynode *poly1,polynode *poly2,polynode *poly)
+//-------add function-------//
+polynode*addpoly(polynode*poly1,polynode*poly2,polynode*poly)
 {
     int comp;
     polynode *a,*b,*temp;
     a=poly1->link;
     b=poly2->link;
-    while (a!=poly1 && b!=poly2)
+    while(a!=poly1&&b!=poly2)
     {
-        if(a->x_exp==b->x_exp && a->y_exp==b->y_exp && a->z_exp==b->z_exp)
+        if(a->x_expo==b->x_expo&&a->y_expo==b->y_expo&&a->z_expo==b->z_expo)
             comp=0;
-        else if(a->x_exp>b->x_exp)
+        else if(a->x_expo>b->x_expo)
+            comp=1;
+        else if(a->x_expo==b->x_expo&&a->y_expo>b->y_expo)
+            comp=1;
+        else if(a->x_expo==b->x_expo&&a->y_expo==b->y_expo&&a->z_expo>b->z_expo)
             comp=1;
         else
-             if(a->x_exp==b->x_exp && a->y_exp>b->y_exp)
-               comp=1;
-        else if(a->x_exp==b->x_exp && a->y_exp==b->y_exp  && a->z_exp>b->z_exp)
-               comp=1 ;
-             else comp= -1;
-        switch (comp)
+            comp=-1;
+        switch(comp)
         {
-            case  0: temp=createnode(a->coeff+b->coeff,a->x_exp,a->y_exp,a->z_exp);
-                     poly=attachnode(temp,poly);
-                     a=a->link;
-                     b=b->link;
-                     break;
-            case  1: temp=createnode(a->coeff,a->x_exp,a->y_exp,a->z_exp);
-                     poly=attachnode(temp,poly);
-                     a=a->link;
-                     break;
-            case -1: temp=createnode(b->coeff,b->x_exp,b->y_exp,b->z_exp);
-                     poly=attachnode(temp,poly);
-                     b=b->link;
-                     break;
+            case 0:temp=create(a->coeff+b->coeff,a->x_expo,a->y_expo,a->z_expo);
+                    poly=attachnode(temp,poly);
+                    a=a->link;
+                    b=b->link;
+                    break;
+            case 1:temp=create(a->coeff,a->x_expo,a->y_expo,a->z_expo);
+                    poly=attachnode(temp,poly);
+                    a=a->link;
+                    break;
+            case -1:temp=create(b->coeff,b->x_expo,b->y_expo,b->z_expo);
+                    poly=attachnode(temp,poly);
+                    b=b->link;
+                    break;
         }
     }
-    while (a!=poly1)
+    while(a!=poly1)
     {
-        temp=createnode(a->coeff,a->x_exp,a->y_exp,a->z_exp);
+        temp=create(a->coeff,a->x_expo,a->y_expo,a->z_expo);
         poly=attachnode(temp,poly);
         a=a->link;
     }
-    while (b!=poly2)
+    while(b!=poly2)
     {
-        temp=createnode(b->coeff,b->x_exp,b->y_exp,b->z_exp);
+        temp=create(b->coeff,b->x_expo,b->y_expo,b->z_expo);
         poly=attachnode(temp,poly);
         b=b->link;
     }
     return poly;
 }
-
-void main()
+int main()
 {
     int ch;
-    polynode *poly1,*poly2,*poly3;
-    //poly1=(polynode*)malloc(sizeof(polynode));
-    //poly2=(polynode*)malloc(sizeof(polynode));
-   poly3=(polynode*)malloc(sizeof(polynode));
-    //poly1->link=poly1;
-    //poly2->link=poly2;
-   poly3->link=poly3;
-    while (1)
+    polynode*poly1,*poly2,*poly3;
+    poly3=(polynode*)malloc(sizeof(polynode));
+    poly3->link=poly3;
+    while(1)
     {
-        printf("\n\n1.Represent & Evaluate a Polynomial\n2.Add 2 Polynomial\n3.Exit\n");
+        printf("\n\t---------MENUE----------\n");
+        printf("\n\t1.Represent and evaluate\n\t2.Add two polynomial\n\t3.exit\n");
+        printf("\nEnter your choice:");
         scanf("%d",&ch);
         switch(ch)
         {
-            case 1: printf("Enter polynomial:\n");
+            case 1:printf("\nEnter the polynomial:\n");
                     poly1=readpoly();
                     display(poly1);
                     evaluate(poly1);
                     break;
-            case 2: printf("Enter polynomial 1:\n");
+            case 2:printf("\nEnter poly1:\n");
                     poly1=readpoly();
                     display(poly1);
-                    printf("\nEnter polynomial 2:\n");
+                    printf("\nEnter poly2:\n");
                     poly2=readpoly();
                     display(poly2);
+                    printf("\nResultent polynomial:\n");
                     poly3=addpoly(poly1,poly2,poly3);
-                    printf("\n\nThe Resultant Polynomial is:\n");
                     display(poly3);
                     break;
-            case 3: exit(0);
-            default: printf("Wrong Choice\n");
+            case 3:
+                    exit(0);
+            default:printf("\nInvalid choice...!\n");
         }
+        
     }
- }
+
+    return 0;
+}
