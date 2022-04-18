d59ba6d509510b49277e1755069e4215494bdf36
RobinKumar5986 <72180986+RobinKumar5986@users.noreply.github.com>
2022-04-18 16:57:55 +0530

    Update program 11.c

--- program 11.c
+++ program 11.c
@@ -1,56 +1,80 @@
-//----BFS DFS operation-----//
-
+//-----BFS DFS operaton-----//
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+//-----------------//
 int a[20][20],q[20],visited[20],reach[10],n,i,j,f=0,r=-1,count=0;
-//----bfs----//
+
+//-------BFS------//
 void bfs(int v)
 {
-    for(i=1;i<=n;i++){
+    for(i=1;i<=n;i++)
         if(a[v][i]&&!visited[i])
-        q[++r]=i;
-        if(f<=r){
-            visited[q[f]]=1;
-            bfs(q[f++]);
+    q[++r]=i;
+    if(f<=r)
+    {
+        visited[q[f]]=1;
+        bfs(q[f++]);
+    }
+}
+//-------DFS-------//
+void dfs(int v)
+{
+    int i;
+    reach[v]=1;
+    for(i=1;i<=n;i++)
+    {
+        if(a[v][i]&&!reach[i])
+        {
+            printf("\n%d->%d",v,i);
+            count++;
+            dfs(i);
         }
     }
 }
-
-int main()
+//------main-------//
+void main()
 {
-    int v,choice;
-    printf("\nEnter the number of CITIES:");
+    int v,ch;
+    printf("\n\tEnter the number of vertex:");
     scanf("%d",&n);
-    //----bfs initilaztion----//
-    for(i=1;i<=n;i++){
+    for(i=1;i<=n;i++)
+    {
         q[i]=0;
         visited[i]=0;
     }
-    //------data input-------//
-    printf("\nEnter the data:\n");
     for(i=1;i<=n;i++)
-    for(j=1;j<=n;j++)
-    scanf("%d",&a[i][j]);
-    printf("1.BFS\n");
-    scanf("%d",&choice);
-    switch(choice){
-        case 1:
-            printf("\nEnter the ROOT vertex:");
-            scanf("%d",&v);
-            bfs(v);
-            if((v<1)||(v>n)){
-                printf("BFS is not possible...!!!");
-            }
-            else{
-                printf("the node which are rechable form %d:",v);
-                for(i=1;i<=n;i++){
-                    if(visited[i])
-                    printf("%d ",i);
+        reach[i]=0;
+    printf("\n\tEnter the graph data in metrix form:\n");
+    for(i=1;i<=n;i++)
+        for(j=1;j<=n;j++)
+            scanf("%d",&a[i][j]);
+    printf("\n\t-----MENUE------\n");
+    printf("\n\t1.BFS\n\t2.DFS\n\t3.EXIT\n");
+    printf("\nEnter your choice:");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+        case 1:printf("\nEnter the starting vertex:");
+                scanf("%d",&v);
+                bfs(v);
+                if((v<1)||(v>n))
+                    printf("\nBFS not possible...!!!");
+                else
+                {
+                    printf("\n\tThe nodes which are reachable from %d is:\n",v);
+                    for(i=1;i<=n;i++)
+                        if(visited[i])
+                            printf("%d\t",i);
                 }
-            }
-            break;
-        default:printf("invalid choice...!!!");
+                break;
+        case 2:dfs(1);
+                if(count==n-1)
+                    printf("\nGraph is connected");
+                else
+                    printf("\nGraph is not connected");
+                break;
+        case 3:
                 exit(0);
+                
     }
-    return 0;
 }
